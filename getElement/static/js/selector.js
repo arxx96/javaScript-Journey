76ad118680f82916fa6d902e6766e7b049330d9d
arxx96 <130518504+arxx96@users.noreply.github.com>
2023-06-10 00:07:11 +0400

    DOM Elements

---
+++ getElement/static/js/selector.js
@@ -0,0 +1,35 @@
+
+
+// var body=document.getElementsByName("body");
+// console.log(body)
+
+// let sec=document.getElementsByClassName("section");
+// console.log(sec)
+
+
+let section2= document.getElementById("section-2");
+// console.log(section2)
+
+
+// //selcting paragraphs
+// //query selector
+// // document.querySelector("p").style.color="red";
+
+
+// //query selector all >>use loop
+// paragraphs=document.querySelectorAll("P")
+// for (var i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].style.color = "blue";
+//   }
+
+
+
+  //###########################
+console.log(section2)
+
+section2.addEventListener("click" , function(e){
+
+    
+    section2.style.color = "red";
+    // section2.style.fontSize = "12rem";
+})
